59f44c31553e63c2aaba0039d822aa31c234dd46
Vasilii Ulitin <albatrumb@gmail.com>
2022-12-21 14:34:42 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketprice = 100; // Цена билета //
+        int condition = 20;    // Расчетная часть на милю //
+        int bonus = ticketprice / condition;
+
+        System.out.println((ticketprice / condition) + " бонусные мили ");
+
+
+    }
+}
